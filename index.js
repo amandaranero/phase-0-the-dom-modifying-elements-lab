b71a5d22936c62eb84430d8e07efd71ebfdda7e4
amanda ranero <amandaranero@gmail.com>
2023-02-02 13:04:44 -0500

    commit tree d49297f6b1e38f11dbacea1fea360056d4adfddd

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Write your code here!
+/* remove main */
+main.remove ();
+
+/*create newHeader var and append it to body */
+
+const newHeader = document.createElement("h1");
+
+
+/* add id to newHeader */
+
+newHeader.id = 'victory'
+
+/*add header text*/
+newHeader.textContent = "Amanda is the champion";
